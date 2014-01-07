85470ac1735c92e43b0ebfbfe1aa19dc5a545a53
Tracy <254458886@qq.com>
2014-01-07 14:13:18 +0800

    Initial Commit

---
+++ UIView+Category/XXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XXAppDelegate.h
+//  UIView+Category
+//
+//  Created by Tracy on 14-1-7.
+//  Copyright (c) 2014年 Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIView+Category/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UIView+Category
+//
+//  Created by Tracy on 14-1-7.
+//  Copyright (c) 2014年 Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XXAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XXAppDelegate class]));
+    }
+}
